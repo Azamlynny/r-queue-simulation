27f4a649fd5bcee3ed878ec26e3acf2ea1820abb
azamlynny <azamlynny@hotmail.com>
2021-04-19 11:49:07 -0400

    Queue dataframe established

--- queue-simulation.R
+++ queue-simulation.R
@@ -11,8 +11,11 @@ simulationMinutes = 720 # 720 minutes = 12 hours
 mainEntry = rpois(simulationMinutes, mainEPM)
 carEntry = rpois(simulationMinutes, carEPM)
 
-totalPeople = sum(mainEntry) + sum(carEntry)
-print(totalPeople)
+mainMealPrice = round(rnorm(sum(mainEntry)) * 3 + 10,2) 
+carMealPrice = round(rnorm(sum(carEntry)) * 3 + 12,2) 
+
+qplot(mainMealPrice)
+qplot(carMealPrice)
 
 dflogs <- data.frame () # Dataframe to track queues
 mainQueue = 0
@@ -21,7 +24,21 @@ sales = 0
 salaries = 0
 profits = 0
 
+dfmain <- data.frame () # Dataframe to track main queue
+dfcar <- data.frame () # Dataframe to track car queue
+mainClientNumber = 1
+carClientNumber = 1
+
+
 for(t in 1:simulationMinutes){
+  
+  if(mainEntry[t] != 0){
+    for(i in 1:mainEntry[t]){
+      dfmain <- rbind(dfmain, c(mainClientNumber, mainMealPrice[mainClientNumber], 'Ordering', t, '', ''))
+      mainClientNumber <- mainClientNumber + 1
+      carClientNumber <- carClientNumber + 1
+    }
+  }
   
   mainQueue <- mainQueue + mainEntry[t]
   carQueue <- carQueue + carEntry[t]
@@ -31,4 +48,9 @@ for(t in 1:simulationMinutes){
   dflogs <- rbind(dflogs, c(t, mainQueue, carQueue, sales, salaries, profits))
 }
 
-colnames(dflogs) <- c('Minute','Main Queue', 'Car Queue')
+colnames(dflogs) <- c('Minute','Main Queue', 'Car Queue', 'Sales', 'Salaries', 'Profits')
+colnames(dfmain) <- c('Client Number','Meal Price', 'State', "Entry Time", "Order Time", "Served Time")
+colnames(dfcar) <- c('Client Number','Meal Price', 'State', "Entry Time", "Order Time", "Served Time")
+
+print(sum(mainEntry))
+
