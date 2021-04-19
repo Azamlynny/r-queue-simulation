08b9b4bae68022ea485b7b744bfdcbdf4b1465d5
azamlynny <azamlynny@hotmail.com>
2021-04-19 10:21:50 -0400

    Implemented iterative dataframe

--- queue-simulation.R
+++ queue-simulation.R
@@ -7,10 +7,28 @@ qplot(serverSkill)
 
 mainEPM = 2 # Main entrance per minute
 carEPM = 1 # Car entrance per minute
-simulationMinutes = 720 # 12 hours
+simulationMinutes = 720 # 720 minutes = 12 hours
 mainEntry = rpois(simulationMinutes, mainEPM)
 carEntry = rpois(simulationMinutes, carEPM)
-qplot(mainEntry)
-qplot(carEntry)
 
-dflogs <- data.frame ()
+totalPeople = sum(mainEntry) + sum(carEntry)
+print(totalPeople)
+
+dflogs <- data.frame () # Dataframe to track queues
+mainQueue = 0
+carQueue = 0
+sales = 0
+salaries = 0
+profits = 0
+
+for(t in 1:simulationMinutes){
+  
+  mainQueue <- mainQueue + mainEntry[t]
+  carQueue <- carQueue + carEntry[t]
+  
+  profits <- sales - salaries 
+  
+  dflogs <- rbind(dflogs, c(t, mainQueue, carQueue, sales, salaries, profits))
+}
+
+colnames(dflogs) <- c('Minute','Main Queue', 'Car Queue')
