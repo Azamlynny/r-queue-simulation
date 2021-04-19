be7fa09d88a4d0a812f4a582afab6bfdc5f29e3c
azamlynny <azamlynny@hotmail.com>
2021-04-19 09:50:35 -0400

    Added poisson queue add per min

--- queue-simulation.R
+++ queue-simulation.R
@@ -4,3 +4,13 @@ library("dplyr")
 serverNumber = 30
 serverSkill = rnorm(serverNumber) + 5 # Generates an array of normal distribution values 
 qplot(serverSkill)
+
+mainEPM = 2 # Main entrance per minute
+carEPM = 1 # Car entrance per minute
+simulationMinutes = 720 # 12 hours
+mainEntry = rpois(simulationMinutes, mainEPM)
+carEntry = rpois(simulationMinutes, carEPM)
+qplot(mainEntry)
+qplot(carEntry)
+
+dflogs <- data.frame ()
